138cef2da840f8d21537b9e35a96d78df90d9d20
JiangHaiJun <zhangzhongbao>
2021-10-16 20:37:09 +0800

    Eureka Initial

---
+++ cloud-eureka-server7001/src/main/java/com/learn/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author : JHJ
+ * @Date : 2021/10/16/20:30
+ * @Description :
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
