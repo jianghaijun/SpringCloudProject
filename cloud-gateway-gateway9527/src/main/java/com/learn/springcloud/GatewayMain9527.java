32a8dd101fbbee3b339f248ec2dd37c1955dc492
JiangHaiJun <zhangzhongbao>
2021-10-24 14:35:34 +0800

    Gateway Initial

---
+++ cloud-gateway-gateway9527/src/main/java/com/learn/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author : JHJ
+ * @Date : 2021/10/24/13:10
+ * @Description :
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/learn/springcloud/filter/GatewayFilter.java
@@ -0,0 +1,34 @@
+package com.learn.springcloud.filter;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author : JHJ
+ * @Date : 2021/10/24/14:23
+ * @Description :
+ */
+@Component
+public class GatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StrUtil.isEmpty(uname)) {
+            System.out.println("用户名非法");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
